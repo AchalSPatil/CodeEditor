efbf6cabbd8ebb0b00f8a811e88d9d4f607eddee
achal-patil-comprinno <141217388+achal-patil-comprinno@users.noreply.github.com>
2023-10-20 16:10:56 +0530

    CodeEditor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [code, setCode] = useState('');
+  const [isLocked, setIsLocked] = useState(false);
+  const [savedCodeBlocks, setSavedCodeBlocks] = useState([]);
+
+  const handleCopyClick = () => {
+    const codeTextArea = document.getElementById('codeTextArea');
+    codeTextArea.select();
+    document.execCommand('copy');
+  };
+
+  const handleSaveClick = () => {
+    if (code.trim() !== '') {
+      setSavedCodeBlocks([...savedCodeBlocks, code]);
+      setCode(''); // Clear the current code editor content after saving
+    }
+  };
+
+  const handleLockClick = () => {
+    setIsLocked(!isLocked);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`code-editor ${isLocked ? 'code-editor--locked' : ''}`}>
+      <textarea
+        id="codeTextArea"
+        className="code-editor__textarea"
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+        readOnly={isLocked}
+      />
+      <div className="code-editor__buttons">
+        <button onClick={handleCopyClick} className="code-editor__copy-button">
+          Copy
+        </button>
+        <button onClick={handleSaveClick} className="code-editor__save-button">
+          Save
+        </button>
+        <button onClick={handleLockClick} className="code-editor__lock-button">
+          {isLocked ? 'Unlock' : 'Lock'}
+        </button>
+      </div>
+
+      {savedCodeBlocks.length > 0 && (
+        <div className="code-editor__saved-code">
+          <h2>Saved Code Blocks</h2>
+          {savedCodeBlocks.map((savedCode, index) => (
+            <pre key={index}>{savedCode}</pre>
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
